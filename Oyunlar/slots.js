53fd0428f91db60d88fe8f6d6cd1d3458854281a
JANO <77622056+Devjano@users.noreply.github.com>
2021-11-06 22:23:55 -0700

    Create slots.js

---
+++ Oyunlar/slots.js
@@ -0,0 +1,89 @@
+const Discord = require('discord.js');
+var ayarlar = require("../ayarlar.json")
+const db = require('quick.db')
+exports.run = function(client, message,  args) {
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+  let miktar = args[0]
+  let para = db.fetch(`para_${message.author.id}`)
+  if(!miktar) return message.channel.send(new Discord.MessageEmbed()
+      .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+      .setColor("GREEN")
+      .setDescription(`**slots** You Can't More Than <a:emoji_10:870059089055592508> 50,000`))
+  //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+if(miktar > 50000) return message.channel.send(new Discord.MessageEmbed()
+      .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+      .setColor("GREEN")
+      .setDescription(`**slots**You Can't More Than <a:emoji_8:870059060714700870> 50,000`))
+  /////////////
+if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
+  .setColor("#ffff00")
+  .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+  .setDescription(`In your wallet to place a bet ${para ? "just " + para + ' <a:emoji_1:870056878464462879> there is!' : 'you have no money!'}`))
+  /////////////////
+  if(isNaN(miktar) || miktar < 0) return message.channel.send(new Discord.MessageEmbed()
+.setColor("RED")
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+.setDescription(`<a:emoji_32:870319275263533107> The amount you entered is not a valid number !?`))
+ let kazandin = miktar*3   
+  let slots = ["<:srandom:878415028510933062>","<:slots2:878401761788977162>","<:slots3:878401708512927774>","<:AAblank_box:872905535698137138>"];
+  //
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+  let result1 = Math.floor((Math.random() * slots.length));
+  let result2 = Math.floor((Math.random() * slots.length));
+  let result3 = Math.floor((Math.random() * slots.length));
+
+  //
+  let ust1 = Math.floor((Math.random() * slots.length));
+  let ust2 = Math.floor((Math.random() * slots.length));
+  let ust3 = Math.floor((Math.random() * slots.length));
+
+  //
+  let alt1 = Math.floor((Math.random() * slots.length));
+  let alt2 = Math.floor((Math.random() * slots.length));
+  let alt3 = Math.floor((Math.random() * slots.length));
+  
+  
+//===BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\ 
+  if (slots[result1] === slots[result2] && slots[result2] === slots[result3] ) {
+    var text2 = `**
+ **__SLOTS__**  
+ <a:slots7:878401859818225706><a:slots7:878401859818225706><a:slots7:878401859818225706>|${message.author.username}
+|          | **`;
+          message.channel.send(text2).then(msg => {
+            setTimeout(() => {
+              msg.edit(`**
+ **__SLOTS__**  
+ ${slots[result1]}${slots[result2]}${slots[result3]}${message.author.username} <:rcash2:878753234175553587> ${kazandin}+
+|          | **`,true);
+            }, 3500);
+          });
+
+     db.add(`para_${message.author.id}`, kazandin)
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+  } else {
+    var text3 = `**
+ **__SLOTS__**  
+ <a:slots7:878401859818225706><a:slots7:878401859818225706><a:slots7:878401859818225706>|${message.author.username}
+|          | **`;
+          message.channel.send(text3).then(msg => {
+            setTimeout(() => {
+              msg.edit(`**
+ **__SLOTS__**  
+ ${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username} <:rcash2:878753234175553587> ${miktar}-
+|          |**`, true);
+            }, 3500);
+          });
+  db.add(`para_${message.author.id}`, -miktar)
+  }
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["slot","s","slotmachine"],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'slots',
+};
