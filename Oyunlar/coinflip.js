416367b2afdf6fc6749e0784f592c71b50f5cd0a
JANO <77622056+Devjano@users.noreply.github.com>
2021-11-06 22:22:40 -0700

    Create coinflip.js

---
+++ Oyunlar/coinflip.js
@@ -0,0 +1,79 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+const ms = require('parse-ms')
+exports.run = async (client, message, args) => {
+  let para = db.fetch(`para_${message.author.id}`) 
+  
+//=== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+  let timeout = 7000;
+  
+  let crime = await db.fetch(`bahisoynama_${message.author.id}`)
+
+      if (crime !== null && timeout - (Date.now() - crime) > 0) {
+        
+        let time = ms(timeout - (Date.now() - crime));
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+        message.channel.send(new Discord.MessageEmbed()
+                      .setColor("#ffff00")
+                        .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+                        .setDescription(`<a:gem2l2:872905739704868954> You Have A Cooldown For __coinflip__
+                        **Time left**
+                        ${time.seconds ? time.seconds + 'seconds' : 'try again!'}`))
+      } else {
+  var miktar = args[0]
+  if(!miktar) return message.channel.send(new Discord.MessageEmbed()
+                                        .setColor("#ffff00")
+                                        .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+                                        .setDescription(`You Can't More Than<a:emoji_40:873255039009968168> 50,000`))
+ if(miktar > 50000) return message.channel.send(new Discord.MessageEmbed()
+                                        .setColor("#ffff00")
+                                        .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+                                        .setDescription(`You can bet up to 50000 <a:emoji_25:870145821469966406>!`))   
+if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+                                              .setColor("#ffff00")
+                                        .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+                                        .setDescription(`In your wallet to place a bet ${para ? "just " + para + ' <a:emoji_25:870145821469966406> there is!' : 'you have no money!'}`))
+        const result = [
+          "LOOSELOOSE",
+          "WINWIN",
+          "LOSSELOSSE",
+          "WINWIN",
+          "LOOSELOOSE"
+        ] 
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+        let awnser = result[Math.floor(Math.random() * result.length)]
+     if (awnser === "LOOSELOOSE") {
+ var kaybettin = miktar*2        
+        //////        
+        var text2 = `**<@${message.author.id}> CoinFlip <a:emoji_12:878402675161591838> Choice win Or Lost**`;
+          message.channel.send(text2).then(msg => {
+            setTimeout(() => {
+              msg.edit(`**<@${message.author.id}>spent cowoncy ${-kaybettin} <:AAcfTails:878401931305967666> and chose heads
+The coin spins...<:AAcfTails:878401931305967666> tail and you lost it all...loose (loose)🔴**`);
+            }, 3000);
+          });
+      await db.set(`bahisoynama_${message.author.id}`, Date.now());   
+      await db.add(`para_${message.author.id}`, -kaybettin);   
+      } else { 
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+          var kazandın = miktar*2
+        var text2 = `**<@${message.author.id}> CoinFlip <a:emoji_12:878402675161591838> Choice win Or Lost**`;
+          message.channel.send(text2).then(msg => {
+            setTimeout(() => {
+              msg.edit(`**<@${message.author.id}> spent cowoncy ${kazandın} <:para1:878402722653675520> and chose heads
+The coin spins...<:para1:878402722653675520> coinflip win (win)🟢**`);
+            }, 3000);
+          });
+    await db.set(`bahisoynama_${message.author.id}`, Date.now());   
+    await db.add(`para_${message.author.id}`, kazandın);    
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+        }}}
+exports.conf = {
+  enabled: true,
+  aliases: ["coinflip"],
+};
+
+exports.help = {
+  name: 'cf',
+};
